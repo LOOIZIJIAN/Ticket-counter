dec4330e61f5893afb179ae6a59f7fd1ce7b2972
LOOIZIJIAN <looizijiann@gmail.com>
2024-01-22 13:34:24 +0800

    add status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,56 +2,64 @@
 import { Button } from "@/components/ui/button";
 import { FcNext } from "react-icons/fc";
 import Link from "next/link";
-import { use, useEffect, useState } from "react";
+import { useState } from "react";
 import { Counter } from "@/components/Counter";
 import toast, { Toaster } from 'react-hot-toast';
 
 export default function Home() {
   const initNum = 1001;
 
-const [ticket, setTicket] = useState<number[]>([]);
-const [lastTic, setLastTic] = useState<number>();
-const [current, setCurrent] = useState<number>(0);
+  const [ticketQueue, setTicketQueue] = useState<number[]>([]);
 
-function handleTicket() {
-  if (ticket.length === 0) {
-    setTicket([initNum]);
-  } else {
-    setTicket((prevNum) => [...prevNum, prevNum[prevNum.length - 1] + 1]);
+  function handleTakeNumber() {
+    const newTicket = generateTicket();
+    setTicketQueue((prevQueue) => {
+      if (prevQueue.length === 4) {
+        const updatedQueue = prevQueue.slice(1);
+        return [...updatedQueue, newTicket];
+      } else {
+        return [...prevQueue, newTicket];
+      }
+    });
+  
+    toast("Your number is " + newTicket);
   }
-}
 
-useEffect(() => {
-  if (ticket.length === current + 4) {
-    setLastTic(ticket[current]);
-    setCurrent((prev) => prev + 1);
-  }
-}, [ticket, current]);
-
-useEffect(()=>{
-  if(ticket.length>0){
-    toast("Your number is " + ticket[ticket.length-1]);
-  }
-},[ticket]);
-
+  const generateTicket = () => {
+    if (ticketQueue.length === 0) {
+      return initNum;
+    } else {
+      return ticketQueue[ticketQueue.length - 1] + 1;
+    }
+  };
 
   return (
     <>
       <Toaster/>
       <header className="flex justify-between m-5">
         <h1 className="text-4xl font-extrabold">Customer View</h1>
-        <Button variant="ghost"><Link className="flex" href={'/manager'}>Manager view<FcNext style={{marginTop: "3.4px", marginLeft: "2px"}}/></Link></Button>
+        <Button variant="ghost"><Link className="flex" href='/manager'>Manager view<FcNext style={{marginTop: "3.4px", marginLeft: "2px"}}/></Link></Button>
       </header>
+
       <div className="container mx-auto">
         <div className="flex items-center flex-col rounded border-2 border-sky-600 py-12 tracking-widest">
           <div className="space-y-4">
-            <div className="text-3xl font-extrabold">Now serving: {ticket[ticket.length-1]}</div>
-            <div className="text-3xl font-extrabold">Last number: {lastTic}</div>
+            <div className="text-3xl font-extrabold">Now serving: {ticketQueue[ticketQueue.length-1] || "None"}</div>
+            <div className="text-3xl font-extrabold">Last Number: {ticketQueue[0] || "None"}</div>
           </div>
-          <div className="mt-16"><Button size="lg" variant="default" onClick={handleTicket}>Take a number</Button></div>
+          <div className="mt-16">
+            <Button size="lg" variant="default" onClick={handleTakeNumber}>
+              Take a number
+            </Button>
+          </div>
         </div>
-        <Counter />
+        <Counter queue={ticketQueue}/>
       </div>
     </>
   );
 }
+
+
+
+
+
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,27 @@
-export function Counter(){
+export function Counter({ queue }: { queue: number[] } ){
+
+  const servingStatus = (counterNumber: number, index: number) => {
+    if(queue.length > 0 && queue[index]!= null && queue[index] === counterNumber){
+      return "bg-red-500";
+    }else{
+      return "bg-green-400";
+    }
+    
+  };
+
   return(
     <div className="flex my-16 justify-between">     
       <div className="border border-sky-900 p-1">
         <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
           <div className="mt-1 flex items-center gap-x-1.5">
             <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-              <div className="h-2 w-2 rounded-full bg-green-400"/>
+              <div className={`h-2 w-2 rounded-full ${servingStatus(queue[0], 0)}`}/>
             </div>                                   
           </div>              
         </div>
         <div className="flex flex-col px-5 pb-5 items-center space-y-2" >             
           <div className="text-2xl font-bold">Counter 1</div>
-          <div></div>              
+          <div>{queue[0]}</div>              
         </div>         
       </div> 
 
@@ -19,13 +29,13 @@ export function Counter(){
         <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
           <div className="mt-1 flex items-center gap-x-1.5">
             <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-              <div className="h-2 w-2 rounded-full bg-green-400"/>
+              <div className={`h-2 w-2 rounded-full ${servingStatus(queue[1], 1)}`}/>
             </div>                                   
           </div>              
         </div>
         <div className="flex flex-col px-5 pb-5 items-center space-y-2" >             
           <div className="text-2xl font-bold">Counter 2</div>
-          <div></div>              
+          <div>{queue[1]}</div>              
         </div>         
       </div>   
 
@@ -33,13 +43,13 @@ export function Counter(){
         <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
           <div className="mt-1 flex items-center gap-x-1.5">
             <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-              <div className="h-2 w-2 rounded-full bg-green-400"/>
+              <div className={`h-2 w-2 rounded-full ${servingStatus(queue[2], 2)}`}/>
             </div>                                   
           </div>              
         </div>
         <div className="flex flex-col px-5 pb-5 items-center space-y-2" >             
           <div className="text-2xl font-bold">Counter 3</div>
-          <div></div>              
+          <div>{queue[2]}</div>              
         </div>         
       </div>           
 
@@ -47,13 +57,13 @@ export function Counter(){
         <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
           <div className="mt-1 flex items-center gap-x-1.5">
             <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-              <div className="h-2 w-2 rounded-full bg-green-400"/>
+              <div className={`h-2 w-2 rounded-full ${servingStatus(queue[3], 3)}`}/>
             </div>                                   
           </div>              
         </div>
         <div className="flex flex-col px-5 pb-5 items-center space-y-2" >             
           <div className="text-2xl font-bold">Counter 4</div>
-          <div></div>              
+          <div>{queue[3]}</div>              
         </div>         
       </div>                   
 
